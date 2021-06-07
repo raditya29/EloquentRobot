94992071b198cbcf6ff5f0c815673c8ea725fbf5
raditya mahardika <raditya.mahardika@outlook.co.id>
2021-06-07 11:53:12 +0700

    clean ups

--- EloquentRobot/DumbRobotFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace EloquentRobot
-{
-    public class DumbRobotFactory : IRobotFactory<DumbRobot>
-    {
-        public DumbRobot Create(string position, Parcel[] parcels) => new DumbRobot(position, Array.Empty<string>(), parcels);
-    }
-}
--- EloquentRobot/IRobotFactory.cs
+++ EloquentRobot/IRobotFactory.cs
@@ -1,9 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System;
 
 namespace EloquentRobot
 {
     public interface IRobotFactory<out TRobot> where TRobot : Robot
     {
         TRobot Create(string position, Parcel[] parcels);
+    }
+
+    public class DumbRobotFactory : IRobotFactory<DumbRobot>
+    {
+        public DumbRobot Create(string position, Parcel[] parcels) => new DumbRobot(position, Array.Empty<string>(), parcels);
+    }
+
+    public class FixedRouteRobotFactory : IRobotFactory<FixedRouteRobot>
+    {
+        public FixedRouteRobot Create(string position, Parcel[] parcels) => new FixedRouteRobot(position, Array.Empty<string>(), parcels);
+    }
+
+    public class GoalOrientedRobotFactory : IRobotFactory<GoalOrientedRobot>
+    {
+        public GoalOrientedRobot Create(string position, Parcel[] parcels) =>
+            new GoalOrientedRobot(position, Array.Empty<string>(), parcels);
+    }
+
+    public class LazyRobotFactory : IRobotFactory<LazyRobot>
+    {
+        public LazyRobot Create(string position, Parcel[] parcels) =>
+            new LazyRobot(position, Array.Empty<string>(), parcels);
     }
 }
